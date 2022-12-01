b1b3ca0eda38045d3c5720746dded336b13fd8c4
Arkadii <arkadiimomot@gmail.com>
2022-12-02 00:40:40 +0200

    Create Department.java

---
+++ backend/src/main/java/com/example/university/model/Department.java
@@ -0,0 +1,47 @@
+package com.example.university.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, columnDefinition="VARCHAR(100)")
+    private String name;
+
+    @Column(nullable = false, columnDefinition="VARCHAR(10)")
+    private String ShortName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortName() {
+        return ShortName;
+    }
+
+    public void setShortName(String shortName) {
+        ShortName = shortName;
+    }
+}
