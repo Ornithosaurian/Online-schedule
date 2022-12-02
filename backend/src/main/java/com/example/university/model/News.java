056ab2afd58fc4d30cd80f9d92cafa6bdd226612
Arkadii <arkadiimomot@gmail.com>
2022-12-02 16:18:37 +0200

    delete geters and seters

--- backend/src/main/java/com/example/university/model/News.java
+++ backend/src/main/java/com/example/university/model/News.java
@@ -27,45 +27,4 @@ public class News {
 
     @Column(nullable = false,columnDefinition="DATE")
     private Date date;
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImgSrc() {
-        return imgSrc;
-    }
-
-    public void setImgSrc(String imgSrc) {
-        this.imgSrc = imgSrc;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
 }
