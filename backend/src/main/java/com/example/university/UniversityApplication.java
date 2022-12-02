7cb4f52db2adf1204829deb6bd994c7c35f4c492
Arkadii <arkadiimomot@gmail.com>
2022-12-02 13:56:53 +0200

    add test data for faculty

--- backend/src/main/java/com/example/university/UniversityApplication.java
+++ backend/src/main/java/com/example/university/UniversityApplication.java
@@ -1,6 +1,8 @@
 package com.example.university;
 
+import com.example.university.model.Faculty;
 import com.example.university.model.News;
+import com.example.university.service.FacultyService;
 import com.example.university.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,12 +18,14 @@ public class UniversityApplication implements CommandLineRunner {
         SpringApplication.run(UniversityApplication.class, args);
     }
 
-// test data for News
+// test data for News, Faculty
     @Autowired
-    public UniversityApplication(NewsService newsService) {
+    public UniversityApplication(NewsService newsService, FacultyService facultyService) {
         this.newsService = newsService;
+        this.facultyService = facultyService;
     }
     private final NewsService newsService;
+    private final FacultyService facultyService;
 
     @Override
     public void run(String... arg) throws Exception {
@@ -45,5 +49,22 @@ public class UniversityApplication implements CommandLineRunner {
 
         newsService.saveNews(news1);
         newsService.saveNews(news2);
+
+        Faculty faculty1 = Faculty.builder()
+                .name("test1")
+                .shortName("T1")
+                .description("firsts description for faculty")
+                .imgSrc("./img/faculty/test1.png")
+                .build();
+
+        Faculty faculty2 = Faculty.builder()
+                .name("test2")
+                .shortName("T2")
+                .description("second description for faculty")
+                .imgSrc("./img/faculty/test2.png")
+                .build();
+
+        facultyService.saveFaculty(faculty1);
+        facultyService.saveFaculty(faculty2);
     }
 }
