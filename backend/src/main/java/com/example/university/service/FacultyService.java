8ed586f3b88c7b8d883e56570c1c17324f792ef8
Arkadii <arkadiimomot@gmail.com>
2022-12-03 11:06:59 +0200

    add Autowired

--- backend/src/main/java/com/example/university/service/FacultyService.java
+++ backend/src/main/java/com/example/university/service/FacultyService.java
@@ -1,6 +1,7 @@
 package com.example.university.service;
 
 import com.example.university.repository.FacultyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -9,6 +10,7 @@ public class FacultyService extends BaseService {
 
     private final FacultyRepository facultyRepository;
 
+    @Autowired
     public FacultyService(FacultyRepository facultyRepository) {
         super(facultyRepository);
         this.facultyRepository = facultyRepository;
